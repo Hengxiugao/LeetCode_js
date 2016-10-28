633f208f6b236a2554adfa875ba32b0851c2b8eb
Gao Hengxiu <Gao Hengxiu>
2016-10-28 00:31:30 -0500

    initial Commit for strStr

--- strStr.js
+++ strStr.js
@@ -1,20 +1,17 @@
-var findSevenAndThree = function(nums) {
-    if(typeof nums != "undefined" && nums !== null && nums.length===0)
-        return;
-    var result = {};
-    result[0] = new Array();
-    result[1] = new Array();
-    result[2] = new Array();
-    for(var i=0;i<nums.length;i++){
-        if(nums[i]%91==0)
-            result[2][result[2].length] = nums[i];
-        else if(nums[i]%7==0)
-            result[1][result[1].length] = nums[i];
-        else if(nums[i]%13==0)
-            result[0][result[0].length] = nums[i];
+var strStr = function(source, target) {
+    if(typeof source == "undefined")
+        return -1;
+    if(typeof target == "undefined" || target.length == 1)
+        return 0;
+    for(var i = 0; i < source.length - target.length + 1; i++){
+        var j = 0;
+        while(j < target.length && source.charAt(i + j) == target.charAt(j)){
+            j++;
+        }
+        if(j == target.length)
+            return i;
     }
-    console.log(result)
-    return result;
+    return -1;
 };
 
-document.write(findSevenAndThree([13,26,91,4,5,6,7,8,9,10,21]));
+document.write(strStr("target", "target"));
